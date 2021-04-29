bfedc3945da4fa557ac9687460ce3164c6b94620
ShahadatPOM <naiem.3112@gmail.com>
2021-04-29 21:17:41 +0600

    login page coded

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,133 +1,70 @@
-@extends('layouts.master-without-nav')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-@section('title')
-    @lang('translation.Login')
-@endsection
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css')}}">
 
-@section('body')
+    <title>Login</title>
+  </head>
+  <body>
+  
 
-    <body>
-    @endsection
+  <div class="d-lg-flex half">
+    <div class="bg order-1 order-md-2" style="background-image: url({{ URL::asset('assets/images/bg_1.jpg')}})"></div>
+    <div class="contents order-2 order-md-1">
 
-    @section('content')
-        <div class="account-pages my-5 pt-sm-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <img src="{{ URL::asset('/front_assets/img/logo.png') }}" alt="" class="mb-3 logo-center" style="display: block;margin-left: auto;margin-right: auto;" height="34">
-                        <div class="card overflow-hidden">
-                            <div class="bg-primary bg-soft">
-                                <div class="row">
-                                    <div class="col-7">
-                                        <div class="text-primary p-4">
-                                            <h5 class="text-primary">Welcome Back !</h5>
-                                            <p>Sign in to continue to Zcon.</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-5 align-self-end">
-                                        <img src="{{ URL::asset('/assets/images/profile-img.png') }}" alt=""
-                                            class="img-fluid">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body pt-0">
-                                <div class="auth-logo">
-                                    <a href="index" class="auth-logo-light">
-                                        <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="{{ URL::asset('/assets/images/logo-light.svg') }}" alt=""
-                                                    class="rounded-circle" height="34">
-                                            </span>
-                                        </div>
-                                    </a>
-
-                                    <a href="index" class="auth-logo-dark">
-                                        <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="{{ URL::asset('/front_assets/img/favicon.png') }}" alt=""
-                                                    class="rounded-circle" height="34">
-                                            </span>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="p-2">
-                                    <form class="form-horizontal" action="{{ url('admin/authenticate') }}" method="post">
-										@csrf
-                                        <div class="mb-3">
-                                            <label for="username" class="form-label">Username</label>
-											<input type="text" class="form-control" id="username" value="" name="username" placeholder="Enter the username">
-                                        </div>
-
-                                        <div class="mb-3">
-                                            <label class="form-label">Password</label>
-                                            <div class="input-group auth-pass-inputgroup">
-												<input type="password" class="form-control" id="password" value="{{ canDisplayCredentials() ? '123456':'' }}" 
-												name="password" placeholder="Enter the password" aria-label="Password" aria-describedby="password-addon">
-                                                
-                                                <button class="btn btn-light " type="button" id="password-addon"><i
-                                                        class="mdi mdi-eye-outline"></i></button>
-                                            </div>
-                                        </div>
-                                        <div class="mt-3 d-grid">
-                                            <button style="background: #4E2465; color: #ffffff" class="btn waves-effect waves-light" type="submit">Log
-                                                In</button>
-                                        </div>
-
-                                        <div class="mt-4 text-center">
-                                            <h5 class="font-size-14 mb-3">Sign in with</h5>
-
-                                            <ul class="list-inline">
-                                                <li class="list-inline-item">
-                                                    <a href="javascript::void()"
-                                                        class="social-list-item bg-primary text-white border-primary">
-                                                        <i class="mdi mdi-facebook"></i>
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item">
-                                                    <a href="javascript::void()"
-                                                        class="social-list-item bg-info text-white border-info">
-                                                        <i class="mdi mdi-twitter"></i>
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item">
-                                                    <a href="javascript::void()"
-                                                        class="social-list-item bg-danger text-white border-danger">
-                                                        <i class="mdi mdi-google"></i>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-
-                                        <div class="mt-4 text-center">
-                                            <a href="auth-recoverpw" class="text-muted"><i
-                                                    class="mdi mdi-lock me-1"></i> Forgot your password?</a>
-                                        </div>
-                                    </form>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="mt-5 text-center">
-
-                            <div>
-                                <p>Don't have an account ? <a href="auth-register" class="fw-medium text-primary">
-                                        Signup now </a> </p>
-                                <p>© <script>
-                                        document.write(new Date().getFullYear())
-
-                                    </script> Zcon. Crafted with <i class="mdi mdi-heart text-danger"></i> Zcon
-                                </p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-7">
+            <h3><strong>Login to Zcon</strong></h3>
+            <div class="welcome-logo">
+              <img src="{{ URL::asset('assets/images/logo.png')}}">
             </div>
-        </div>
-        <!-- end account-pages -->
+            <form action="{{ url('admin/authenticate') }}" method="post">
+                @csrf
+              <div class="form-group first">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" value="" name="username" placeholder="Enter the username">
+              </div>
+              <div class="form-group last mb-3">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" value="{{ canDisplayCredentials() ? '123456':'' }}" 
+                placeholder="Enter the password" aria-label="Password" aria-describedby="password-addon">
+              </div>
+              
+              <div class="d-flex mb-5 align-items-center">
+                <label class="control control--checkbox mb-0"><span class="caption"><input type="checkbox" /> Remember me</span>
 
-    @endsection
-@section('script')
+                </label>
+                <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
+              </div>
+
+              <input type="submit" value="Log In" class="btn btn-block btn-primary">
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+    
+    
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" ></script>
+    <script src="{{ URL::asset('assets/js/images/main.js')}}"></script>
+
+
+
 <script>
 	$(document).ready(function() {
 		var canDisplayCredentials = '{!! canDisplayCredentials() !!}';
@@ -167,4 +104,5 @@
 		}
 	});
 	</script>
-@endsection
+</body>
+</html>
